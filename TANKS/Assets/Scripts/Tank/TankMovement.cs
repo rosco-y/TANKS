c79c5f98e18e751d44fad357ffb028208f7c16b1
rosco-y <ross.ylitalo@gmail.com>
2020-09-29 08:29:26 -0400

    Tank is moving and audio is working.

--- TANKS/Assets/Scripts/Tank/TankMovement.cs
+++ TANKS/Assets/Scripts/Tank/TankMovement.cs
@@ -10,7 +10,7 @@ public class TankMovement : MonoBehaviour
     public AudioClip m_EngineDriving;      
     public float m_PitchRange = 0.2f;
 
-    /*
+    //*
     private string m_MovementAxisName;     
     private string m_TurnAxisName;         
     private Rigidbody m_Rigidbody;         
@@ -25,9 +25,10 @@ public class TankMovement : MonoBehaviour
     }
 
 
+    // after Awake(), before Update()s.
     private void OnEnable ()
     {
-        m_Rigidbody.isKinematic = false;
+        m_Rigidbody.isKinematic = false; // no forces can be applied to Kinematic rigidbodies.
         m_MovementInputValue = 0f;
         m_TurnInputValue = 0f;
     }
@@ -46,34 +47,70 @@ public class TankMovement : MonoBehaviour
 
         m_OriginalPitch = m_MovementAudio.pitch;
     }
-    */
+    //*/
 
     private void Update()
     {
         // Store the player's input and make sure the audio for the engine is playing.
+        m_MovementInputValue = Input.GetAxis(m_MovementAxisName);
+        m_TurnInputValue = Input.GetAxis(m_TurnAxisName);
+        EngineAudio();
     }
 
 
     private void EngineAudio()
     {
         // Play the correct audio clip based on whether or not the tank is moving and what audio is currently playing.
+
+        // if we're sitting still....
+        if (Mathf.Abs(m_MovementInputValue) < 0.1f && Mathf.Abs(m_TurnInputValue) < 0.1f)
+        {
+            if (m_MovementAudio.clip == m_EngineDriving)
+                setEngineAudioClip(m_EngineIdling);
+        }
+        else
+        {
+            if (m_MovementAudio.clip == m_EngineIdling)
+                setEngineAudioClip(m_EngineDriving);
+        }
     }
 
 
     private void FixedUpdate()
     {
         // Move and turn the tank.
+        Move();
+        Turn();
+
+       
+    }
+
+    void setEngineAudioClip(AudioClip clip)
+    {
+        m_MovementAudio.clip = clip;
+        m_MovementAudio.pitch = Random.Range(m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
+        m_MovementAudio.Play();
     }
 
 
     private void Move()
     {
         // Adjust the position of the tank based on the player's input.
+        Vector3 movement = transform.forward * m_MovementInputValue * m_Speed * Time.deltaTime;
+
+        m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
     }
 
 
     private void Turn()
     {
         // Adjust the rotation of the tank based on the player's input.
+
+        float turn = m_TurnInputValue * m_TurnSpeed * Time.deltaTime;
+
+        Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
+
+        m_Rigidbody.MoveRotation(m_Rigidbody.rotation * turnRotation);
+
     }
 }
